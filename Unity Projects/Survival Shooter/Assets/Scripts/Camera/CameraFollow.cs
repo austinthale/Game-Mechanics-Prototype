4097479099da65494b7da65e619e240502b98abc
Unknown <bbqdpickles@gmail.com>
2017-02-02 14:12:04 -0700

    Adding Tanks, Roll a Ball, and Survival Shooter

---
+++ Unity Projects/Survival Shooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;   // initial value is 5
+
+	Vector3 offset;
+
+	void Start()
+	{
+		offset = transform.position - target.position;
+	}
+
+	// follows a physics update. If we just did update, the timing of following would be different.
+	void FixedUpdate()
+	{
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
---
+++ Unity Projects/Survival Shooter/Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    void Start ()
+    {
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+		// takes a random number to create the spawn point index
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+		// create an instance of an object
+        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
